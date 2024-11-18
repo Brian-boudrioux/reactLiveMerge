00ac1c72ecee3b3a322ad8f4b5a3a11953f5784d
Brian Boudrioux <brian.boudrioux@wildcodeschool.com>
2024-11-18 15:40:14 +0100

    search filters done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import SearchFilters from "./components/SearchFilters";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Header />
-			<main>{/* todo list and filter here */}</main>
+			<main>
+				<SearchFilters />
+			</main>
 			<Footer />
 		</>
 	);
---
+++ src/components/SearchFilters.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function SearchFilters() {
+
+  const [searchName, setSearchName] = useState("");
+  const [cards, setCards] = useState([]);
+
+
+  function search(e) {
+    setSearchName(e.target.value);
+    fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${e.target.value}`)
+    .then((response) => response.json())
+    .then((data) => setCards(data));
+  }
+
+  return (
+    <div>
+      <input type="text" placeholder="Votre recherche" value={searchName} onInput={search} />
+    </div>
+  )
+}
+
+export default SearchFilters;

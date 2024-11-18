0b75071c6550c28ad7413c8f71db6591107f20a2
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-11-18 16:21:36 +0100

    Fix where data comes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 			<Navbar />
 			<Header />
 			<main>
-				<CardList />
+				<CardList cards={cards}/>
 			</main>
 			<Footer />
 		</>
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,16 +1,7 @@
 import { useEffect, useState } from "react";
 import CardDetails from "./CardDetails";
 
-function CardList() {
-    const [cards, setCards] = useState([]);
-    
-    // TODO : Magic number 10 will evolve with other features
-    useEffect(() => {
-        fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0')
-        .then((response) => response.json())
-        .then(({data}) => setCards(data))
-    }, [])
-
+function CardList({cards}) {
     return(
         <>
             <div className="card">
